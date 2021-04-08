f68b12529370554b0f53aa974e292dbf0ceb3a78
azizyildiz4257 <azizyildiz4257@hotmail.com>
2021-04-08 23:10:20 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri No : " + musteri.Musteri_No);
+            Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyadi);
+            Console.WriteLine("Müşteri Şehir : " + musteri.Sehir);
+            Console.WriteLine("Müşteri Ekleme İşlemi Başarıyla Gerçekleştirildi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri No : " + musteri.Musteri_No);
+            Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyadi);
+            Console.WriteLine("Müşteri Şehir : " + musteri.Sehir);
+            Console.WriteLine("Müşteri Ekleme İşlemi Başarıyla Gerçekleştirildi.");
+        }
+
+        public void Listele(Musteri[] musteris)
+        {
+            foreach (var customers in musteris)
+            {
+                Console.WriteLine("Müşteri No : " + customers.Musteri_No);
+                Console.WriteLine("Müşteri Adı : " + customers.Adi);
+                Console.WriteLine("Müşteri Soyadı : " + customers.Soyadi);
+                Console.WriteLine("Müşteri Şehir : " + customers.Sehir);
+            }
+        }
+    }
+}
